7cc1445b3bb87c7b9cd87d2df6173997125daabb
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-21 11:53:39 -0700

    implemented input and expected spaceship

--- tests/web.rs
+++ tests/web.rs
@@ -11,3 +11,24 @@ wasm_bindgen_test_configure!(run_in_browser);
 fn pass() {
     assert_eq!(1 + 1, 2);
 }
+
+extern crate wasm_game_of_life;
+use wasm_game_of_life::Universe;
+
+#[cfg(test)]
+pub fn input_spaceship() -> Universe {
+    let mut universe = Universe::new();
+    universe.set_width(6);
+    universe.set_height(6);
+    universe.set_cells(&[(1, 2), (2, 3), (3, 1), (3, 2), (3, 3)]);
+    universe
+}
+
+#[cfg(test)]
+pub fn expected_spaceship() -> Universe {
+    let mut universe = Universe::new();
+    universe.set_width(6);
+    universe.set_height(6);
+    universe.set_cells(&[(2, 1), (2, 3), (3, 2), (3, 3), (4, 2)]);
+    universe
+}
