9741077ab9498801184d557e83731028945b0398
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-20 21:03:41 -0700

    implemented universe

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,36 @@ pub enum Cell {
     Dead = 0,
     Alive = 1,
 }
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    // Uses deltas and modulo to avoid the special case of the edges
+    // of the universe.
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+}
