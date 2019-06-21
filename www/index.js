4d53225d9f7b083fb9406b1e502bd63f0322cd68
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-20 23:01:30 -0700

    linter

--- www/index.js
+++ www/index.js
@@ -1,12 +1,12 @@
-import { Universe, Cell } from "wasm-game-of-life";
+import { Universe, Cell } from 'wasm-game-of-life';
 
 // Import the WebAssembly memory at the top of the file.
-import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
 
 const CELL_SIZE = 5; // px
-const GRID_COLOR = "#CCCCCC";
-const DEAD_COLOR = "#FFFFFF";
-const ALIVE_COLOR = "#000000";
+const GRID_COLOR = '#CCCCCC';
+const DEAD_COLOR = '#FFFFFF';
+const ALIVE_COLOR = '#000000';
 
 // Construct the universe, and get its width and height.
 const universe = Universe.new();
@@ -15,33 +15,24 @@ const height = universe.height();
 
 // Give the canvas room for all of our cells and a 1px border
 // around each of them.
-const canvas = document.getElementById("game-of-life-canvas");
+const canvas = document.getElementById('game-of-life-canvas');
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext('2d');
-
-const renderLoop = () => {
-  universe.tick();
-
-  drawGrid();
-  drawCells();
-
-  requestAnimationFrame(renderLoop);
-};
 
 const drawGrid = () => {
   ctx.beginPath();
   ctx.strokeStyle = GRID_COLOR;
 
   // Vertical lines.
-  for (let i = 0; i <= width; i++) {
+  for (let i = 0; i <= width; i += 1) {
     ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
     ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
   }
 
   // Horizontal lines.
-  for (let j = 0; j <= height; j++) {
+  for (let j = 0; j <= height; j += 1) {
     ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
     ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
   }
@@ -49,9 +40,7 @@ const drawGrid = () => {
   ctx.stroke();
 };
 
-const getIndex = (row, column) => {
-  return row * width + column;
-}
+const getIndex = (row, column) => row * width + column;
 
 const drawCells = () => {
   const cellsPtr = universe.cells();
@@ -59,8 +48,8 @@ const drawCells = () => {
 
   ctx.beginPath();
 
-  for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
+  for (let row = 0; row < height; row += 1) {
+    for (let col = 0; col < width; col += 1) {
       const idx = getIndex(row, col);
 
       ctx.fillStyle = cells[idx] === Cell.Dead
@@ -77,6 +66,15 @@ const drawCells = () => {
   }
 
   ctx.stroke();
+};
+
+const renderLoop = () => {
+  universe.tick();
+
+  drawGrid();
+  drawCells();
+
+  requestAnimationFrame(renderLoop);
 };
 
 drawGrid();
