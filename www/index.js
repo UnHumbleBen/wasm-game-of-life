e1afe85dde020a4fd8a22df33d67b936dffd9aee
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-20 22:07:58 -0700

    fixed spelling error

--- www/index.js
+++ www/index.js
@@ -1,6 +1,6 @@
 import { Universe } from "wasm-game-of-life";
 
-const pre = document.getElementById("game-of-life-cnavas");
+const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
 
 const renderLoop = () => {
