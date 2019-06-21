c8075934e32858c998d4c4a53fa004feca5e919c
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-20 22:41:39 -0700

    refractoring canvas

--- www/index.js
+++ www/index.js
@@ -1,11 +1,23 @@
 import { Universe } from "wasm-game-of-life";
 
-const pre = document.getElementById("game-of-life-canvas");
+// Construct the universe, and get its width and height.
 const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
 
 const renderLoop = () => {
-  pre.textContent = universe.render();
   universe.tick();
+
+  drawGrid();
+  drawCells();
 
   requestAnimationFrame(renderLoop);
 };
