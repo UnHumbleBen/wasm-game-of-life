aa9d77aa998a2c15c6cfba8fd7c43567696baab0
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-20 22:57:54 -0700

    implemented add cells

--- www/index.js
+++ www/index.js
@@ -1,4 +1,12 @@
-import { Universe } from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
+
+// Import the WebAssembly memory at the top of the file.
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
 
 // Construct the universe, and get its width and height.
 const universe = Universe.new();
@@ -41,4 +49,36 @@ const drawGrid = () => {
   ctx.stroke();
 };
 
+const getIndex = (row, column) => {
+  return row * width + column;
+}
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new UintBArray(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead
+        ? DEAD_COLOR
+        : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE,
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+drawGrid();
+drawCells();
 requestAnimationFrame(renderLoop);
