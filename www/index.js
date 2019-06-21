85f5bc7a139a153fd26757ce8b1ea3878e7dae5f
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-21 15:19:14 -0700

    added frame per second timer

--- www/index.js
+++ www/index.js
@@ -70,14 +70,57 @@ const drawCells = () => {
   ctx.stroke();
 };
 
+const fps = new class {
+  constructor() {
+    this.fps = document.getElementById('fps');
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure of
+    // frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    // eslint-disable-next-line no-shadow
+    const fps = 1 / delta * 100;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i += 1) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    const mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+    `.trim();
+  }
+}();
+
 let animationId = null;
 
 // This function is the same as before, except the
 // result of `requestAnimationFrame` is assigned to
 // `animationId`.
 const renderLoop = () => {
-  // eslint-disable-next-line no-debugger
-  debugger;
+  fps.render(); // new
   universe.tick();
 
   drawGrid();
